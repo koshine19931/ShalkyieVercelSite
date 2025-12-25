5d2a5995df28b151ff109891afec544a4294e0c6
koshine19931 <koshine19931@gmail.com>
2025-12-25 21:09:37 +0630

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,7 +1,12 @@
-export default function handler(req, res) {
+module.exports = (req, res) => {
   const { host } = req.headers;
   const clientID = process.env.GITHUB_CLIENT_ID;
   const redirectUri = `https://${host}/api/callback`;
+  
+  if (!clientID) {
+      return res.status(500).send("Error: GITHUB_CLIENT_ID is missing in Vercel settings.");
+  }
+
   const url = `https://github.com/login/oauth/authorize?client_id=${clientID}&scope=repo,user&redirect_uri=${redirectUri}`;
   res.redirect(url);
-}
+};
